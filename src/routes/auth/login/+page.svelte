9d415ecaa45e91639c6aa33d40ae278ec3031a7b
Bence Tóth <bnctth@protonmail.com>
2022-10-29 16:54:59 +0000

    login

--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -4,5 +4,10 @@
     let password='';
 </script>
 <form action="?/login" use:enhance>
-
+<label for="username">Username</label>
+<input type="text" id="username" bind:value={username}/>
+<label for="password">Password</label>
+<input type="password" id="password" bind:value={password}/>
+<submit>Login</submit>
+<a href="../signup">If you are a new user...</a>
 </form>
