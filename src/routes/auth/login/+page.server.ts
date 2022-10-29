1001a10b0099f2880581b5a3344169261e36cf8c
Bence Tóth <bnctth@protonmail.com>
2022-10-29 16:35:50 +0000

    login

---
+++ src/routes/auth/login/+page.server.ts
@@ -0,0 +1,7 @@
+import type { Actions } from './$types';
+ 
+export const actions: Actions = {
+  login: async (event) => {
+    // TODO log the user in
+  }
+};
---
+++ src/routes/auth/login/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { enhance } from "$app/forms";
+    let username='';
+    let password='';
+</script>
+<form action="?/login" use:enhance>
+
+</form>
