65fb81924b1fe986586d4aac47f27714d387bcd5
Bence Tóth <bnctth@protonmail.com>
2022-10-29 18:40:33 +0000

    signup

---
+++ src/routes/auth/signup/+page.server.ts
@@ -0,0 +1,41 @@
+import type { Actions } from './$types';
+import { SignJWT } from 'jose';
+import { invalid } from '@sveltejs/kit';
+import { prisma } from '$lib/prisma';
+import { compare, genSalt, hash } from 'bcrypt';
+import { env } from '$env/dynamic/private';
+import { dev } from '$app/environment';
+import { invalidateAll } from '$app/navigation';
+
+export const actions: Actions = {
+	login: async ({ request, cookies }) => {
+		const data = await request.formData();
+		const name = data.get('name') as string;
+		const username = data.get('username') as string;
+		const password = data.get('password') as string;
+		if (!name) {
+			return invalid(400, { name, missingName: true });
+		}
+		if (!username) {
+			return invalid(400, { username, missingUsername: true });
+		}
+		if (!password) {
+			return invalid(400, { password, missingPassword: true });
+		}
+		let user = await prisma.user.findFirst({
+			where: { username }
+		});
+		if (user) {
+			return invalid(403, { alreadyTaken: true });
+		}
+		const passwordHash = await hash(password, await genSalt(10));
+		user = await prisma.user.create({ data: { name, username, passwordHash } });
+		const token = await new SignJWT({ name: user.name, username: user.username, id: user.id })
+			.setIssuedAt()
+			.setProtectedHeader({ alg: 'HS256' })
+			.sign(new TextEncoder().encode(env.SECRET));
+		cookies.set('token', token, { secure: !dev });
+		invalidateAll();
+		return;
+	}
+};
---
+++ src/routes/auth/signup/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import type { ActionData } from './$types';
+	export let form: ActionData;
+</script>
+
+<form action="?/login" method="POST" use:enhance>
+	<label for="name">Name</label>
+	<input name="name" type="text" id="name" />
+	{#if form?.missingName}You need to give a name{/if}
+	<label for="username">Username</label>
+	<input name="username" type="text" id="username" />
+	{#if form?.missingUsername}You need to give a username{/if}
+	<label for="password">Password</label>
+	<input name="password" type="password" id="password" />
+	{#if form?.missingPassword}You need to give a password{/if}
+	<button type="submit">Login</button>
+	{#if form?.alreadyTaken}Username is already taken.{/if}
+</form>
+<a href="../signup">If you already have an account..</a>
