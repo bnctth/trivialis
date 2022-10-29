5e2c81d104fd9ec0e326044bbf8459e3b6e2c93c
Bence Tóth <bnctth@protonmail.com>
2022-10-29 20:38:46 +0000

    model page

---
+++ src/routes/[username]/[collectionid]/[modelid]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+	$: selected = data.collection.models.findIndex((m) => m.id == +data.modelid);
+</script>
+
+{data.collection.owner.name}
+{data.collection.title}
+{data.collection.models[selected].title}
+{data.collection.models[selected].tags}
+<iframe src={data.collection.models[selected].link} />
+{#each data.collection.models as model, i (model.id)}
+	<a href="./{model.id}">
+		{#if i == selected} a kiválasztottra vonatkozik csak{/if}
+		{model.title}
+	</a>
+{/each}
