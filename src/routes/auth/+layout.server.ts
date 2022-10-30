e0b20f5e4ebacc2904da22e4e5e3a82412ad8ed0
Bence Tóth <bnctth@protonmail.com>
2022-10-30 15:11:15 +0000

    redirect to username

--- src/routes/auth/+layout.server.ts
+++ src/routes/auth/+layout.server.ts
@@ -1,5 +1,5 @@
 import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 export const load: LayoutServerLoad = ({ locals }) => {
-	if (locals.user) throw redirect(302, '/');
+	if (locals.user) throw redirect(302, `/me`);
 };
